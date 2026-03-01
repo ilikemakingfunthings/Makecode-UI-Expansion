0c65081325df871debeb3462e8b5288f5aa0bb17
UI Expansion Dev <dev@makecode-ui.dev>
2026-02-28 20:23:34 -0600

    Add simple cursor creation block

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-// Empty
+// @ts-nocheck
+
+//% color="#FF6B35" icon="\uf245"
+namespace ui {
+    //% block="create cursor with $img"
+    //% img.shadow=screen_image_picker
+    export function createCursor(img: Image): Sprite {
+        return sprites.create(img)
+    }
+}
